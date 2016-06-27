232a68ed03a5b3c38ba915769ef3473beee7acbe
Vikrant Goel <vgoel1@walmartlabs.com>
2016-06-26 21:43:45 -0700

    only pawn left now '

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -68,26 +68,21 @@ public class AlphaBetaChess {
 	}
 
 	private static String possibleB(int i) {
-		StringBuilder list = new StringBuilder();
 
-		return list.toString();
-	}
-
-	private static String possibleQ(int i) {
 		StringBuilder list = new StringBuilder();
 		char oldPiece;
 
 		int distance = 1;
 		int r = i/8, c = i%8;
 
-		//eight combinations for eight directions our queen can go to
+		//eight combinations for eight directions our bishop can go to
 		for(int j = -1; j <= 1; j++){
 			for(int k = -1; k <= 1; k++){
-				
-				//for j=0 and k=0, we are basically not moving at all
-				if(j==0 && k==0)
+
+				//so that it only moves diagonally
+				if(j==0 || k==0)
 					continue;
-				
+
 				//for cases where we are going out of the board
 				try{
 					distance = 1;
@@ -112,7 +107,70 @@ public class AlphaBetaChess {
 						distance++;
 					}
 					if(Character.isLowerCase(chessBoard[r+distance*j][c+distance*k])){
-						
+
+						//suppose we make the switch
+						oldPiece = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = chessBoard[r][c];
+						chessBoard[r][c] = ' ';
+
+						//if valid move record it
+						if(kingSafe()){
+							//valid move
+							list.append(String.valueOf(r) + String.valueOf(c) + String.valueOf(r+distance*j) 
+							+ String.valueOf(c+distance*k) + String.valueOf(oldPiece));
+						}
+
+						//switch it back to be able to calculate more moves
+						chessBoard[r][c] = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = oldPiece;
+					}
+
+				}catch(Exception e){ }
+			}
+		}
+		return list.toString();
+	}
+
+	private static String possibleQ(int i) {
+		StringBuilder list = new StringBuilder();
+		char oldPiece;
+
+		int distance = 1;
+		int r = i/8, c = i%8;
+
+		//eight combinations for eight directions our queen can go to
+		for(int j = -1; j <= 1; j++){
+			for(int k = -1; k <= 1; k++){
+
+				//for j=0 and k=0, we are basically not moving at all
+				if(j==0 && k==0)
+					continue;
+
+				//for cases where we are going out of the board
+				try{
+					distance = 1;
+					while(chessBoard[r+distance*j][c+distance*k] == ' '){
+
+						//suppose we make the switch
+						oldPiece = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = chessBoard[r][c];
+						chessBoard[r][c] = ' ';
+
+						//if valid move record it
+						if(kingSafe()){
+							//valid move
+							list.append(String.valueOf(r) + String.valueOf(c) + String.valueOf(r+distance*j) 
+							+ String.valueOf(c+distance*k) + String.valueOf(oldPiece));
+						}
+
+						//switch it back to be able to calculate more moves
+						chessBoard[r][c] = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = oldPiece;
+
+						distance++;
+					}
+					if(Character.isLowerCase(chessBoard[r+distance*j][c+distance*k])){
+
 						//suppose we make the switch
 						oldPiece = chessBoard[r+distance*j][c+distance*k];
 						chessBoard[r+distance*j][c+distance*k] = chessBoard[r][c];
@@ -141,10 +199,10 @@ public class AlphaBetaChess {
 		char oldPiece;
 		int r = i/8, c = i%8;
 
-		//eight combinations for eight directions our queen can go to
+		//eight combinations for eight directions our king can go to
 		for(int j = -1; j <= 1; j++){
 			for(int k = -1; k <= 1; k++){
-				
+
 				//for cases where we are going out of the board
 				try{
 					//we will not move on one of our own player
@@ -169,21 +227,116 @@ public class AlphaBetaChess {
 				}catch(Exception e){ }
 			}
 		}
-		
+
 		//TODO add castling logic
 		return list.toString();
 	}
 
 	private static String possibleR(int i) {
-		StringBuilder list = new StringBuilder();
 
+		StringBuilder list = new StringBuilder();
+		char oldPiece;
+
+		int distance = 1;
+		int r = i/8, c = i%8;
+
+		//eight combinations for eight directions our rook can go to
+		for(int j = -1; j <= 1; j++){
+			for(int k = -1; k <= 1; k++){
+
+				//
+				if(!((j==0 && k!=0) || (j!=0 && k==0)))
+					continue;
+
+				//for cases where we are going out of the board
+				try{
+					distance = 1;
+					while(chessBoard[r+distance*j][c+distance*k] == ' '){
+
+						//suppose we make the switch
+						oldPiece = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = chessBoard[r][c];
+						chessBoard[r][c] = ' ';
+
+						//if valid move record it
+						if(kingSafe()){
+							//valid move
+							list.append(String.valueOf(r) + String.valueOf(c) + String.valueOf(r+distance*j) 
+							+ String.valueOf(c+distance*k) + String.valueOf(oldPiece));
+						}
+
+						//switch it back to be able to calculate more moves
+						chessBoard[r][c] = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = oldPiece;
+
+						distance++;
+					}
+					if(Character.isLowerCase(chessBoard[r+distance*j][c+distance*k])){
+
+						//suppose we make the switch
+						oldPiece = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = chessBoard[r][c];
+						chessBoard[r][c] = ' ';
+
+						//if valid move record it
+						if(kingSafe()){
+							//valid move
+							list.append(String.valueOf(r) + String.valueOf(c) + String.valueOf(r+distance*j) 
+							+ String.valueOf(c+distance*k) + String.valueOf(oldPiece));
+						}
+
+						//switch it back to be able to calculate more moves
+						chessBoard[r][c] = chessBoard[r+distance*j][c+distance*k];
+						chessBoard[r+distance*j][c+distance*k] = oldPiece;
+					}
+
+				}catch(Exception e){ }
+			}
+		}
 		return list.toString();
 	}
 
 	private static String possibleK(int i) {
-		StringBuilder list = new StringBuilder();
 
+		StringBuilder list = new StringBuilder();
+		char oldPiece;
+
+		int r = i/8, c = i%8;
+
+		//eight combinations for eight directions our king can go to
+		for(int j = -2; j <= 2; j++){
+			for(int k = -2; k <= 2; k++){
+
+				//so that it only 2.5 spaces diagonally
+				if(!(j!=k && j!=(-1)*k && j!=0 && k!=0))
+					continue;
+
+				//for cases where we are going out of the board
+				try{
+					if (!Character.isUpperCase(chessBoard[r+j][c+k])){
+					
+						//suppose we make the switch
+						oldPiece = chessBoard[r+j][c+k];
+						chessBoard[r+j][c+k] = chessBoard[r][c];
+						chessBoard[r][c] = ' ';
+
+						//if valid move record it
+						if(kingSafe()){
+							//valid move
+							list.append(String.valueOf(r) + String.valueOf(c) + String.valueOf(r+j) 
+							+ String.valueOf(c+k) + String.valueOf(oldPiece));
+						}
+
+						//switch it back to be able to calculate more moves
+						chessBoard[r][c] = chessBoard[r+j][c+k];
+						chessBoard[r+j][c+k] = oldPiece;
+					}
+
+				}catch(Exception e){ }
+			}
+		}
 		return list.toString();
+
 	}
 
 	private static String possibleP(int i) {
